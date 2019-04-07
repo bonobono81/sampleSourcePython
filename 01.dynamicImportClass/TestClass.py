2cf5da41ab088cb4b16536414357ef2071c43528
bonobono81 <topbluesky@gmail.com>
2019-04-07 17:15:42 +0900

    Create TestClass.py

---
+++ 01.dynamicImportClass/TestClass.py
@@ -0,0 +1,9 @@
+
+class TestClass:
+    def print01():
+        print("classPrint01Call and reutrn 1")
+        return 1;
+        
+    def print02():
+        print("classPrint01Call and reutrn 2")
+        return 2;        
